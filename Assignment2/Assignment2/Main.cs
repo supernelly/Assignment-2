a53f81da2b780acf09293e3dae6cda1ef4cc0187
supernelly <nelsonsu13@yahoo.ca>
2018-10-31 12:05:54 -0400

    d
    d

---
+++ Assignment2/Assignment2/Main.cs
@@ -0,0 +1,23 @@
+﻿/*
+ *  Assignment 2 - Huffman Codes
+ *
+ *  Written By: Nelson Su (0616242) - November 2018
+ *
+ *  Description here
+ *  
+ */
+using System;
+
+namespace Assignment2
+{
+    public static class Driver
+    {
+        public static void Main()
+        {
+            // Test code here
+            Huffman S = new Huffman(" ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
+            Console.ReadKey();
+
+        }
+    }
+}
